553b8b724fce0cd68f45d2edfe8a53ea799766aa
tritiatimmins <tritialtimmins@gmail.com>
2016-02-19 19:51:47 -0800

    created tables in the sql file

--- server/schema.sql
+++ server/schema.sql
@@ -4,8 +4,22 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER PRIMARY KEY,
+  users.id INTEGER,
+  rooms.id INTEGER,
+  msg TEXT
 );
 
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  username TEXT
+
+);
+
+CREATE TABLE rooms (
+  id INTEGER PRIMARY KEY,
+  roomname TEXT
+);
 /* Create other tables and define schemas for them here! */
 
 
